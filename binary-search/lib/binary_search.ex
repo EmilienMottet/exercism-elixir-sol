0db59f1ff0a2f00ce166bd4ab1ed03b1a0b5363f
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-11-22 11:03:05 +0100

    use Tuple instead of List

--- binary-search/lib/binary_search.ex
+++ binary-search/lib/binary_search.ex
@@ -20,17 +20,17 @@ defmodule BinarySearch do
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
   def search(numbers, key) when is_tuple(numbers) do
     max = tuple_size(numbers) - 1
-    do_search(Tuple.to_list(numbers), key, 0, max)
+    do_search(numbers, key, 0, max)
   end
 
   defp do_search(_numbers, _key, min_index, max_index) when min_index > max_index do
     :not_found
   end
 
-  defp do_search(numbers, key, min_index, max_index) when is_list(numbers) do
+  defp do_search(numbers, key, min_index, max_index) when is_tuple(numbers) do
     index = div(min_index + max_index, 2)
 
-    case Enum.at(numbers, index) do
+    case elem(numbers, index) do
       ^key -> {:ok, index}
       target when target > key -> do_search(numbers, key, min_index, index - 1)
       target when target < key -> do_search(numbers, key, index + 1, max_index)
